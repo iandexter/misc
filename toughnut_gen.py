8ef4a037079c8012f62aae38fb82aca1ee75dae4
Ian Dexter R Marquez <iandexter+tech@gmail.com>
2013-11-14 17:02:41 +0800

    Compute entropy based on algorithm used.

--- toughnut_gen.py
+++ toughnut_gen.py
@@ -25,6 +25,10 @@ seed = [
 refreshed.',
 'password',
 'random',
+'tough nut',
+'cracker',
+'correct horse battery staple',
+'2 Corinthians 8-9'
 ]
 
 ### Variables for password generation
@@ -97,19 +101,32 @@ def generate(str = None, k = 0, tmin = 0, tmax = 0):
     if len(''.join(n)) < t:
         d = t - len(''.join(n))
         n.append(pad(False, d, string.punctuation + string.digits))
-    return ''.join(n)
+    n = ''.join(n)
+    return [n, entropy(n)]
 
-def xkcd_gen(word_list = []):
+def xkcd_gen(word_list = [], wmin = 0, wmax = 0):
     """ Generate passphrase based on xkcd/936 """
     random.shuffle(word_list)
-    return ' '.join(w.strip() for w in word_list[:random.randrange(3,5)])
+    nw = random.randrange(wmin,wmax)
+    p = ' '.join(w.strip() for w in word_list[:nw])
+    return [p, xkcd_entropy(p, len(word_list), nw)]
 
-def entropy(str = None, word_list = []):
+def xkcd_entropy(str = None, len_list = 0, num_words = 0):
+    i = len_list
+    for j in range(num_words):
+        len_list -= j
+        i *= len_list
+    return log(i, 2)
+
+def entropy(str = None):
     N = 0
     if str.isalpha():
-        N += len(string.ascii_lowercase)
+        if str.islower() or str.isupper():
+            N += len(string.ascii_lowercase)
+        else:
+            N += len(string.ascii_letters)
     elif str.isalnum():
-        N += len(string.ascii_lowercase + string.digits)
+        N += len(string.ascii_letters + string.digits)
     else:
         N += len(string.ascii_letters + string.digits + string.punctuation)
     return len(str) * log(N,2)
@@ -126,22 +143,31 @@ if __name__ == '__main__':
     seed_files = sys.argv[2:]
     seed_list = readfiles(seed_files)
 
-
     password_list = []
-    for i in range(int(r/3)):
-        password_list.append(random.choice(seed_list))
-    for i in range(int(r/3)):
+    ### Plain-text
+    for i in range(int(r/4)):
+        p = random.choice(seed_list)
+        password_list.append([p, entropy(p)])
+    ### Character substitutions
+    for i in range(int(r/4)):
         s = random.choice(seed_list)
         if len(s) < tmax:
             d = random.randint(tmin,tmax)
             s += pad(False, d, string.digits)
-        password_list.append(s)
-    for i in range(int(r/3)):
+        password_list.append([s, entropy(s)])
+    ### Character substitutions + padding
+    for i in range(int(r/4)):
         s = generate(random.choice(seed_list), k, tmin, tmax)
         password_list.append(s)
-    for i in range(int(r/3)):
-        password_list.append(xkcd_gen(seed_list))
+    ### XKCD scheme
+    for i in range(int(r/4)):
+        password_list.append(xkcd_gen(seed_list, 3, 5))
 
-    max_width = len(max(password_list, key=len))
+    p_list = []
     for p in password_list:
-        print "%-*s %4d %8.2f" % (max_width, p, len(p), entropy(p, seed_list))
+        p_list.append(p[0])
+    max_width = len(max(p_list, key=len))
+    from pprint import pprint
+    password_list.sort(key=lambda x: x[1])
+    for p in password_list:
+        print "%-*s %4d %8.2f" % (max_width, p[0], len(p[0]), p[1])
